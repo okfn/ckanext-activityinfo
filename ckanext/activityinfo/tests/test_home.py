5af48357ffaea9fc713a0513e82bdec836a45a0d
avdata99 <andres@data99.com.ar>
2025-11-24 12:54:48 -0300

    fix for CKAN 2.10

--- ckanext/activityinfo/tests/test_home.py
+++ ckanext/activityinfo/tests/test_home.py
@@ -1,5 +1,6 @@
 from types import SimpleNamespace
 import pytest
+from ckan.plugins import toolkit
 from ckantoolkit.tests import factories as ckan_factories
 from ckanext.activityinfo.tests import factories
 
@@ -22,13 +23,21 @@ class TestActivityInfoUI:
 
         resp = app.get("/activity-info", headers=environ)
         assert resp.status_code == 200
+        if toolkit.check_ckan_version(min_version="2.11"):
+            response_text = resp.text
+        else:
+            response_text = resp.unicode_body
         # CKAN users without a registered ActivityInfo token, will be asked to add one
-        assert "Add API key" in resp.text
+        assert "Add API key" in response_text
 
     def test_activityinfo_user(self, app, setup_data):
         environ = {"Authorization": setup_data.activityinfo_user["token"]}
 
         resp = app.get("/activity-info", headers=environ)
         assert resp.status_code == 200
+        if toolkit.check_ckan_version(min_version="2.11"):
+            response_text = resp.text
+        else:
+            response_text = resp.unicode_body
         # Activity info users will see the ActivityInfo UI
-        assert "Update API key" in resp.text
+        assert "Update API key" in response_text
